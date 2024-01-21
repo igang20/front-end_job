1d10006cb94159b324b56712791061c3598cd872
Ilya Iovva <ilichzavr@gmail.com>
2024-01-21 14:50:09 +0200

    new branch

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//     ПАНЕЛЬ С ФИЛЬТРАМИ
+
+let sityFilter = document.getElementById("locationFilter");
+
+let busyFilter = document.getElementById("typeOfBusyFilter");
+
+let workTimeFilter = document.getElementById("workTimeFilter");
+
+let expFilter = document.getElementById("WorkExpFilter");
+
+let salaryFilter = document.getElementById("salaryFilter");
+
+let submintForm = document.getElementById("submitFilterForm");
+
+
+//     ----------------------
