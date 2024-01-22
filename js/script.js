6ab495a848c3337ff4e235e6eeb6481ed376d37a
Ilya <65218566+igang20@users.noreply.github.com>
2024-01-22 22:24:36 +0200

    nav modification
    Реализация выпадающего меню в панели фильтров, начало разработки класса формы для отправки запросов на сервер

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 
 let sityFilter = document.getElementById("locationFilter");
 
-let busyFilter = document.getElementById("typeOfBusyFilter");
-
 let workTimeFilter = document.getElementById("workTimeFilter");
+
+let busyFilter = document.getElementById("typeOfBusyFilter");
 
 let expFilter = document.getElementById("WorkExpFilter");
 
@@ -14,14 +14,71 @@ let submintForm = document.getElementById("submitFilterForm");
 
 let filtresOpenerNode = document.querySelectorAll(".filtresOpener");
 
-// console.log(filtresOpenerNode);
-// for (const filtresopenerItem of filtresOpenerNode) {
-//   console.log(filtresopenerItem.checked);
-// }
-function displayFilterPanel(ev) {
-  ev.preventDefault();
-}
-busyFilter.addEventListener("click", displayFilterPanel(event));
+let busyFilterStatus = false;
 
-workTimeFilter.addEventListener;
-//           ----------------------
+let workTimeFilterStatus = false;
+
+let expFilterStatus = false;
+
+busyFilter.addEventListener("click", () => {
+    if (busyFilterStatus == false) {
+        busyFilter.querySelector(".dropDownMenu").classList.add("visible");
+        busyFilterStatus += true;
+    } else if (busyFilterStatus == true) {
+        busyFilter.querySelector(".dropDownMenu").classList.remove("visible");
+        busyFilterStatus = false;
+    }
+});
+
+workTimeFilter.addEventListener("click", () => {
+    if (workTimeFilterStatus == false) {
+        workTimeFilter.querySelector(".dropDownMenu").classList.add("visible");
+        workTimeFilterStatus += true;
+    } else if (workTimeFilterStatus == true) {
+        workTimeFilter.querySelector(".dropDownMenu").classList.remove("visible");
+        workTimeFilterStatus = false;
+    }
+});
+
+expFilter.addEventListener("click", () => {
+    if (expFilterStatus == false) {
+        expFilter.querySelector(".dropDownMenu").classList.add("visible");
+        expFilterStatus += true;
+    } else if (expFilterStatus == true) {
+        expFilter.querySelector(".dropDownMenu").classList.remove("visible");
+        expFilterStatus = false;
+    }
+});
+
+
+// Класс с формой для отправки на сервер
+class filterform {
+    constructor(
+        sity,
+        flexDay,
+        fullDay,
+        remoteWork,
+        // fullBusy,
+        // participalBusy,
+        // projectWork,
+        // studyCours,
+        // noExp,
+        // oneToThreeYearsExp,
+        // threeToSixYearsExp,
+        // moreSixYearsParametr,
+        // salary
+    ) {
+        this.sity = sity;
+        this.workTimeFilter = {
+            flexDay: this.flexDay = flexDay,
+            fullDay: this.fullDay = fullDay,
+            remoteWork: this.remoteWork = remoteWork,
+        }
+
+
+    }
+}
+
+let flterParametrs = new filterform('москва', true, false, false)
+console.log(flterParametrs)
+    //           ----------------------
