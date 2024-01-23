0e2d79f7cc794b5d2bbdea1aa3f28772da0917a4
iontrxne <maksimus.kozhaev@mail.ru>
2024-01-23 21:36:32 +0300

    add createCard function

--- max-branch/cards.js
+++ max-branch/cards.js
@@ -1,15 +1,42 @@
-class Card {
-  constructor(logo, name, vacancy, region, filters, date, isliked) {
-    logo = this.logo;
-    name = this.name;
-    vacancy = this.vacancy;
-    region = this.region;
-    filters = this.filters;
-    isliked = isliked;
+class VacancyCard {
+  constructor (companyName, position, location, //filters,
+  date, isLiked = false) {
+    this.companyName = companyName;
+    this.position = position;
+    this.location = location;
+    //this.filters = filters;
+    this.date = date;
+    this.isLiked = isLiked;
   }
 
-  // displayCard() {
-  //  document.getElementById(bigCard).innerHTML = 
-  //}
-}
-
+  displayCard() {
+    const newCard = document.createElement('div');
+    newCard.classList.add('card')
+    newCard.innerHTML = 
+    `
+    <img src="./img/unimaged.svg" alt="internet-people logo">
+    <div class="vacancyInfo">
+      <div class="companyInfo">
+        <span>${this.companyName}</span>
+        <h3>${this.position}</h3>
+        <span>${this.location}</span>
+      </div>
+      <div id="filtersContainer" class="filters">
+        <span id="jQueryFilter">jQuery</span>
+        <span id="JavaScriptFilter">JavaScript</span>
+        <span id="CSS3Filter">CSS3</span>
+        <span id="ReactFilter">React</span>
+        <span id="GitFilter">Git</span>
+        <span id="VueFilter">Vue</span>
+        <span id="FlexboxFilter">Flexbox</span>
+        <span id="HTML5Filter">HTML5</span>
+      </div>
+    </div>
+    <div class="cardAttr">
+      <img src="./img/like-on.svg" alt="favorite" class="isLiked">
+      <span id="vacancyDate">${this.date}</span>
+    </div>
+  `;
+  document.getElementById('cardsSection').appendChild(newCard)
+  }
+} // без фильтров
--- max-branch/script.js
+++ max-branch/script.js
@@ -1,20 +1,47 @@
-const toggleFavorite = (element) => {
-  if (element.classList.contains('unLiked')) {
-    element.src = "./img/like-on.svg";
-    element.alt = "favorite";
-    element.classList.remove('unLiked');
-    element.classList.add('isLiked');
-  } else if (element.classList.contains('isLiked')) {
-    element.src = "./img/like-off.svg";
-    element.alt = "unfavorite";
-    element.classList.remove('isLiked');
-    element.classList.add('unLiked');
-  }
-};
+//const toggleFavorite = (element) => {
+//  if (element.classList.contains('unLiked')) {
+//    element.src = "./img/like-on.svg";
+//    element.alt = "favorite";
+//    element.classList.remove('unLiked');
+//    element.classList.add('isLiked');
+//  } else if (element.classList.contains('isLiked')) {
+//    element.src = "./img/like-off.svg";
+//    element.alt = "unfavorite";
+//    element.classList.remove('isLiked');
+//    element.classList.add('unLiked');
+//  }
+//};
+// сделать объектную модель и передавать параметры isLiked == True
+//
+//
+//document.addEventListener('click', (event) => {
+//  const clickedElement = event.target;
+//  if (clickedElement.classList.contains('isLiked') || clickedElement.classList.contains('unLiked')) {
+//    toggleFavorite(clickedElement);
+//  }
+//});
 
-document.addEventListener('click', (event) => {
-  const clickedElement = event.target;
-  if (clickedElement.classList.contains('isLiked') || clickedElement.classList.contains('unLiked')) {
-    toggleFavorite(clickedElement);
-  }
-});
+
+const createVacancyButton = document.getElementById('createVacancyButton');
+
+createVacancyButton.onclick = () => {
+
+  const newVacancyCard = new VacancyCard(
+    companyInput = document.getElementById('companyInput').value,
+    positionInput = document.getElementById('positionInput').value,
+    locationInput = document.getElementById('locationInput').value,
+    dateInput = document.getElementById('dateInput').value,
+    true
+  )
+
+  // тут надо преобразовать дату
+
+  if (companyInput !== "" && 
+      positionInput !== "" && 
+      locationInput !== "" &&
+      dateInput !== "") {
+    newVacancyCard.displayCard();
+    } else {
+      alert('Введите данные в форму')
+    }
+}
