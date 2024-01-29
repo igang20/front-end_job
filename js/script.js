fe853093acd585e7b189292fd4bca7c99e4a73c8
Ilya <65218566+igang20@users.noreply.github.com>
2024-01-29 22:11:11 +0200

    fetching of filter form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //     ПАНЕЛЬ С ФИЛЬТРАМИ
 
-let sityFilter = document.getElementById("locationFilter");
+let sityFilter = document.querySelector("#locationFilter input");
 
 let workTimeFilter = document.getElementById("workTimeFilter");
 
@@ -8,11 +8,11 @@ let busyFilter = document.getElementById("typeOfBusyFilter");
 
 let expFilter = document.getElementById("WorkExpFilter");
 
-let salaryFilter = document.getElementById("salaryFilter");
+let salaryFilter = document.querySelector("#salaryFilter input");
 
 let submintForm = document.getElementById("submitFilterForm");
 
-let filtresOpenerNode = document.querySelectorAll(".filtresOpener");
+let inputParentsNode = document.querySelectorAll('.filterItem input')
 
 let busyFilterStatus = false;
 
@@ -21,37 +21,67 @@ let workTimeFilterStatus = false;
 let expFilterStatus = false;
 
 busyFilter.addEventListener("click", () => {
-  if (busyFilterStatus == false) {
-    busyFilter.querySelector(".dropDownMenu").classList.add("visible");
-    busyFilterStatus += true;
-  } else if (busyFilterStatus == true) {
-    busyFilter.querySelector(".dropDownMenu").classList.remove("visible");
-    busyFilterStatus = false;
-  }
+    if (busyFilterStatus == false) {
+        busyFilter.querySelector(".dropDownMenu").classList.add("visible");
+        busyFilterStatus += true;
+    } else if (busyFilterStatus == true) {
+        busyFilter.querySelector(".dropDownMenu").classList.remove("visible");
+        busyFilterStatus = false;
+    }
 });
 
 workTimeFilter.addEventListener("click", () => {
-  if (workTimeFilterStatus == false) {
-    workTimeFilter.querySelector(".dropDownMenu").classList.add("visible");
-    workTimeFilterStatus += true;
-  } else if (workTimeFilterStatus == true) {
-    workTimeFilter.querySelector(".dropDownMenu").classList.remove("visible");
-    workTimeFilterStatus = false;
-  }
+    if (workTimeFilterStatus == false) {
+        workTimeFilter.querySelector(".dropDownMenu").classList.add("visible");
+        workTimeFilterStatus += true;
+    } else if (workTimeFilterStatus == true) {
+        workTimeFilter.querySelector(".dropDownMenu").classList.remove("visible");
+        workTimeFilterStatus = false;
+    }
 });
 
 expFilter.addEventListener("click", () => {
-  if (expFilterStatus == false) {
-    expFilter.querySelector(".dropDownMenu").classList.add("visible");
-    expFilterStatus += true;
-  } else if (expFilterStatus == true) {
-    expFilter.querySelector(".dropDownMenu").classList.remove("visible");
-    expFilterStatus = false;
-  }
+    if (expFilterStatus == false) {
+        expFilter.querySelector(".dropDownMenu").classList.add("visible");
+        expFilterStatus += true;
+    } else if (expFilterStatus == true) {
+        expFilter.querySelector(".dropDownMenu").classList.remove("visible");
+        expFilterStatus = false;
+    }
 });
 
-submintForm.addEventListener("click", () => {
-    
-});
+submintForm.addEventListener('click', () => {
+    let filterform = new Object();
+
+    for (let inputItem of inputParentsNode) {
+        if (inputItem.checked === true)
+            filterform[inputItem.name] = inputItem.checked;
+    }
+
+    filterform.sityFilter = sityFilter.value;
+
+    filterform.salaryFilter = salaryFilter.value
+
+    fetch("http://ip.jsontest.com/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(filterform)
+    })
+
+    busyFilterStatus = false;
+    workTimeFilterStatus = false;
+    expFilterStatus = false;
+    busyFilter.querySelector(".dropDownMenu").classList.remove("visible");
+    workTimeFilter.querySelector(".dropDownMenu").classList.remove("visible");
+    expFilter.querySelector(".dropDownMenu").classList.remove("visible");
+
+})
+
+console.log(sityFilter)
+
+
+
 
 //           ----------------------
